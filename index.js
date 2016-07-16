755811d0d5e65518bd391e55c66a953805240623
Mauricio Montalvo <mauricio@wizeservices.com>
2016-07-16 13:42:45 -0500

    Changes server port

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ require('./model/db');
 
 var app = express();
 
-app.set('port', (process.env.PORT || 5000));
+app.set('port', (process.env.PORT || 80));
 
 app.listen(app.get('port'), function() {
   console.log('Node app is running on port', app.get('port'));
