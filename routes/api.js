b271e378d25d255eb9d27e256985b26ba6e0713c
Mauricio Montalvo <mauricio@wizeline.com>
2016-11-11 23:37:13 -0600

    Removes moonki stuff

--- routes/api.js
+++ routes/api.js
@@ -1,142 +1,24 @@
 var express = require('express');
 var router = express.Router();
-var mongoose = require('mongoose');
-var Request = require('../model/request');
-var Q = require('q');
-var fs = require('fs');
-var time = require('time');
+var request = require('request');
 
-/* GET home page. */
-router.get('/list', function(req, res) {
-    var promise = getList2();
+router.post('/cxsa', function(req, res, next) {
+  var url = '';
+  var params = req.body;
 
-    promise.then(
-        function(collection){
-            res.json(collection);
-        },
-        function(){
-          res.json({'message': 'Error'});
-        }
-    );
-});
-
-router.get('/save', function(req, res) {
-    console.log(req);
-    req.query['time'] = getDate();
-    var promise = save2(req.query);
-
-    promise.then(
-        function(shortUrl){
-          res.json(shortUrl);
-        },
-        function(){
-            res.writeHead('Content-Type','text/html; charset=utf-8')
-            res.end('adas');
-        }
-    );
-});
-
-router.post('/save', function(req, res) {
-    var params = req.body;
-    console.log(params);
-
-    params['time'] = getDate();
-    var promise = save2(params);
-
-    promise.then(
-        function(shortUrl){
-          res.json(shortUrl);
-        },
-        function(){
-            res.writeHead('Content-Type','text/html; charset=utf-8')
-            res.end('adas');
-        }
-    );
-});
-
-function getDate() {
-  var now = new time.Date();
-  now.setTimezone("America/Mexico_City");
-
-  return now.toString();
-}
-
-function getList(){
-
-  var deferred = Q.defer();
-  var query = Request.find({});
-
-  query.exec(function (err, collection){
-    if(err) {
-      deferred.reject();
-    }
-    else {
-      deferred.resolve(collection);
-    }
-  });
-
-    return deferred.promise;
-}
-
-function getList2() {
-  var deferred = Q.defer();
-  var data = getList3();
-
-  if(data) {
-    deferred.resolve(data);
+  for(var key in params) {
+    url += (key + '=' + params[key] + '&');
   }
-  else {
-    deferred.reject();
-  }
-  return deferred.promise;
-}
-
-function getList3() {
-  return require('../files/data.json');
-}
-
-function save(object){
-
-    var deferred = Q.defer();
-    var news = new Request({data: JSON.stringify(object)});
-
-    news.save(function (err)
-    {
-        if (err){
-            deferred.reject();
-        }
-        else{
-            deferred.resolve(news);
-        }
+  res.status(400).send(); return;
+  request('http://199.195.116.177/nomas/SendMessageController.php?' + url, 
+    function (error, response, body) {
+      if(error || body !== '1') {
+        res.status(400).send();
+      } else {
+        res.send();
+      }
     });
-
-    return deferred.promise;
-}
-
-function save2(object) {
-  var deferred = Q.defer();
-  var filePath = './files/data.json';
-  var data = getList3();
-
-  while(data.length >= 15) {
-    data.shift();
-  }
-
-  data.push(object);
-  data = JSON.stringify(data);
-
-  fs.writeFile(filePath, data, function(error) {
-    if(error) {
-      //console.log('Error writing: ' + filePath);
-      console.log(error);
-      deferred.reject();
-    }
-    else {
-      deferred.resolve(object);
-    }
   });
 
-  return deferred.promise;
-}
 
 module.exports = router;
--- routes/index.js
+++ routes/index.js
@@ -1,38 +1,17 @@
 var express = require('express');
 var router = express.Router();
-var request = require('request');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Awesome Moonki Webapp' });
+  res.render('index', { title: ':)' });
 });
 
 router.get('/health', function (req, res) {
-
-  res.send({ server: 'Moonki Webapp', status: 'true', vir: '0.1', ip: req.connection.remoteAddress });
+  res.send({ server: 'Mauricio', status: 'true', vir: '0.1', ip: req.connection.remoteAddress });
 });
 
 router.get('/myv', function(req, res, next) {
   res.render('template');
 });
-
-router.post('/cxsa', function(req, res, next) {
-
-  var url = '';
-  var params = req.body;
-
-  for(var key in params) {
-    url += (key + '=' + params[key] + '&');
-  }
-  res.status(400).send(); return;
-  request('http://199.195.116.177/nomas/SendMessageController.php?' + url, 
-    function (error, response, body) {
-      if(error || body !== '1') {
-        res.status(400).send();
-      } else {
-        res.send();
-      }
-    });
-  });
 
 module.exports = router;
