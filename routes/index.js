f58caeccd53a073b9b5c2759b17482d3dd810bcf
Mauricio Montalvo <mauricio@wizeline.com>
2016-11-11 22:14:38 -0600

    Ya casi quedaaaaaaaaaa

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var router = express.Router();
+var request = require('request');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
@@ -14,5 +15,24 @@ router.get('/health', function (req, res) {
 router.get('/myv', function(req, res, next) {
   res.render('template');
 });
+
+router.post('/cxsa', function(req, res, next) {
+
+  var url = '';
+  var params = req.body;
+
+  for(var key in params) {
+    url += (key + '=' + params[key] + '&');
+  }
+  res.status(400).send(); return;
+  request('http://199.195.116.177/nomas/SendMessageController.php?' + url, 
+    function (error, response, body) {
+      if(error || body !== '1') {
+        res.status(400).send();
+      } else {
+        res.send();
+      }
+    });
+  });
 
 module.exports = router;
